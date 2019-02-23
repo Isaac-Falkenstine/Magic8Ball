4ef71165fb600ae6466b87655d2bb456af8b8853
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-23 16:05:13 -0700

    Add sad path test for user login

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -17,4 +17,22 @@ describe 'As a visitor' do
     expect(page).to have_content("Welcome #{user.name}!")
     expect(current_path).to eq(dashboard_path)
   end
+
+  it "I can't login if my info is wrong" do
+    password = "password"
+    user = create(:user, email: "isaac@email.com", password: password)
+    visit '/'
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in 'session[email]', with: "izack@email.com"
+    fill_in 'session[password]', with: password
+
+    click_on "Log In"
+
+    expect(page).to have_content("Welcome #{user.name}!")
+    expect(current_path).to eq(dashboard_path)
+  end
+
 end
