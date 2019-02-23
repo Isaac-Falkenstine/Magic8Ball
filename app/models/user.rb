fe0480090275e27d63b4f0ffe1d3cd9624bb9f1a
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-23 14:52:03 -0700

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
 
+  has_many :advices
+
+  validates_presence_of :name,
+                        :email
+
+  validates :email, uniqueness: true
+
+  has_secure_password validations: false
+
 end
