45a4b7f630140cdea672bfa09ed92d922a6ea7a4
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-24 13:26:22 -0700

    Create AdviceService test

---
+++ spec/services/advice_service_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'AdviceService' do
+  it "exists" do
+    service = AdviceService.new
+
+    expect(service).to be_a(AdviceService)
+  end
+
+  context "instance methods" do
+    context "#results_json" do
+      it "returns a array of 5 link objects" do
+        service = AdviceService.new
+
+        expect(service.search_results("friends")).to be_a(Hash)
+        expect(service.search_results("friends")).to have_key(:total_results)
+        expect(service.search_results("friends")).to have_key(:slips)
+        expect(service.search_results("friends")[:slips].first).to have_key(:advice)
+        expect(service.search_results("friends")[:slips].first).to have_key(:slip_id)
+      end
+    end
+  end
+end
