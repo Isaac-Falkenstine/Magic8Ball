c71768c80bc6f7a764ffb6cd1c8e717316657e83
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-25 13:30:34 -0700

    Add shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,8 @@ DatabaseCleaner.strategy = :truncation
 
 
 RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
