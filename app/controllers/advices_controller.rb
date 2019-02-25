6cb41421182de8424d4ed3a8bca78310bfa19486
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-25 10:43:56 -0700

    Make advices controller

---
+++ app/controllers/advices_controller.rb
@@ -0,0 +1,12 @@
+class AdvicesController < ApplicationController
+
+  def create
+    advice_user = params.to_unsafe_h.key("").split("!!!")
+    user = User.find_by(email: advice_user.last)
+    advice = Advice.new(body: advice_user.first, user_id: user.id)
+    if advice.save
+      flash[:notice] = "Advice Saved!"
+      redirect_to dashboard_path
+    end
+  end
+end
